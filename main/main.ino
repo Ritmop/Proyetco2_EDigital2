8d0322fd0851bb0a587d928eb1054b19ae0ec97e
Ritmop <125711101+Ritmop@users.noreply.github.com>
2023-10-05 23:42:32 -0600

    Update main.ino
    Se agregouna pantalla de menu e implemento un control para estado del videojuego.

--- main/main.ino
+++ main/main.ino
@@ -18,9 +18,18 @@
 Player helicopter1(64, 32, prueba);
 Player helicopter2(64, 32, prueba);
 
+typedef enum GameStateType
+{
+  menu = 0,
+  playing = 1,
+  gameOver = 2
+} GameStateType;
+
+GameStateType gameStatus = menu;
+
 int tiempo = 0;
 
-int global_y = 100;
+
 void setup() {
   //Hardware setup
   pinMode(PUSH1, INPUT_PULLUP);
@@ -32,43 +41,71 @@ void setup() {
   SPI.setModule(3); //SPI for LCD
   Serial.println("Inicio");
   LCD_Init();
-
-  //Background
-  LCD_Clear(0x0000);
-  for (int x = 0; x < 319;) {
-    LCD_Bitmap(x, 1, 16, 16, tile2);
-    LCD_Bitmap(x, 17, 16, 16, tile);
-
-    LCD_Bitmap(x, 207, 16, 16, tile);
-    LCD_Bitmap(x, 223, 16, 16, tile);
-    x += 16;
-  }
-  //Player init
-  helicopter1.set_to(10, 100);
-  helicopter2.set_to(80, 100);
-  //helicopter3.set_to(160, 50);
-
-
-
-  delay(1000);
+  delay(10);
 }
 
 void loop() {
-  if (!digitalRead(PUSH1)) {
-    helicopter1.leap(tiempo);
-    helicopter2.leap(tiempo);
-    //helicopter1.move_right();
+  if (gameStatus == menu) {
+    draw_menu();
+    while (true) {
+      if (!digitalRead(PUSH1)) {
+        Serial.print(gameStatus);
+        gameStatus = playing;
+        break;
+      }
+    }
   }
-  if (!digitalRead(PUSH2)) {
-    while(!digitalRead(PUSH2));
-    helicopter1.shoot(tiempo, 16,16,enemy);
-    helicopter2.shoot(tiempo, 16,16,enemy);
+  else if (gameStatus == playing) {
+    draw_backgroud();
+    while (true) {
+      execute_game();
+    }
   }
-  helicopter1.free_fall(tiempo);
-  helicopter1.move_proj(tiempo);
-  helicopter1.update_display();  
-  helicopter2.free_fall(tiempo);
-  helicopter2.move_proj(tiempo);
-  helicopter2.update_display();
-  tiempo += 2;
+}
+
+
+
+void draw_menu() {
+  LCD_Clear(0x2800);
+  LCD_Print("HELICOPTERS", 80, 20, 2, 0xFC08, 0x2800);
+  LCD_Print("OPTION 1", 80, 110, 2, 0x0000, 0x2800);
+  LCD_Print("OPTION 2", 80, 130, 2, 0x0000, 0x2800);
+  LCD_Print("OPTION 3", 80, 150, 2, 0x0000, 0x2800);
+}
+
+void draw_backgroud() {
+  LCD_Clear(0x0000);
+  for (int x = 0; x < 319;) {
+    LCD_Bitmap(x, 0, 16, 16, tile2);
+    LCD_Bitmap(x, 16, 16, 16, tile);
+    LCD_Bitmap(x, 207, 16, 16, tile);
+    LCD_Bitmap(x, 223, 16, 16, tile2);
+    x += 15;
+  }
+}
+
+void execute_game() {  
+  helicopter1.set_to(15, 100);
+  helicopter2.set_to(15, 120);
+  delay(1000);
+
+  while (true) {
+    if (!digitalRead(PUSH1)) {
+      helicopter1.leap(tiempo);
+      helicopter2.leap(tiempo);
+      //helicopter1.move_right();
+    }
+    if (!digitalRead(PUSH2)) {
+      while (!digitalRead(PUSH2));
+      helicopter1.shoot(tiempo, 16, 16, enemy);
+      helicopter2.shoot(tiempo, 16, 16, enemy);
+    }
+    helicopter1.free_fall(tiempo);
+    helicopter1.move_proj(tiempo);
+    helicopter1.update_display();
+    helicopter2.free_fall(tiempo);
+    helicopter2.move_proj(tiempo);
+    helicopter2.update_display();
+    tiempo += 2;
+  }
 }
